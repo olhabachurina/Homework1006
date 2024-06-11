a31a5d8b6d20af0a7c1752aa847930b5d150a413
Olga Bachurina <bachuri12@ukr.net>
2024-06-11 23:26:25 +0300

    Add project files.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Homework1006.Models;
+namespace Homework1006.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var topMovies = await _context.Movies.Take(12).ToListAsync();
+            return View(topMovies);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Homework1006.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Получаем строку подключения из файла конфигурации
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст MovieContext в качестве сервиса в приложение
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(connection));
+
+// Добавляем сервисы MVC
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles(); // обрабатывает запросы к файлам в папке wwwroot
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
+
+app.Run();
+
